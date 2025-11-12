46199f0269237d51a122f11943986380f1f24dd9
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2025-11-12 11:21:35 +0530

    Add ReadFromFile class to read content from a file

---
+++ Read_From_File.java
@@ -0,0 +1,18 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFromFile {
+    public static void main(String[] args) {
+        try {
+            FileReader reader = new FileReader("example.txt");
+            int ch;
+            while ((ch = reader.read()) != -1) {
+                System.out.print((char) ch);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading the file.");
+            e.printStackTrace();
+        }
+    }
+}
